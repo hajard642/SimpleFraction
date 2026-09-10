485b5ecb24bfef484a803c76376e718355cd3f38
Hajar Drai <hajardraiz@gmail.com>
2026-09-10 16:54:23 +0200

    Ajout des attributs de Fraction

--- src/main/java/org/example/Fraction.java
+++ src/main/java/org/example/Fraction.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Fraction {
-
+    private int numerator;
+    private int denominator;
     @Override
     public String toString() {
         return "Je suis une fraction.";
