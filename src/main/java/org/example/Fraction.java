72a23950a2c182083fbbdc58b3a55b79d6aab2fb
Hajar Drai <hajardraiz@gmail.com>
2026-09-10 17:27:29 +0200

    Ajout des getters

--- src/main/java/org/example/Fraction.java
+++ src/main/java/org/example/Fraction.java
@@ -3,8 +3,31 @@ package org.example;
 public class Fraction {
     private int numerator;
     private int denominator;
+    public static final Fraction ZERO = new Fraction(0, 1);
+    public static final Fraction UN = new Fraction(1, 1);
     @Override
     public String toString() {
         return "Je suis une fraction.";
+    }
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(int numerator) {
+        this.numerator = numerator;
+        this.denominator = 1;
+    }
+
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,25 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 public class Main {
+
     public static void main(String[] args) {
-        Fraction fraction = new Fraction();
-        System.out.println(fraction.toString());
+
+        Fraction fraction1 = new Fraction(2, 3);
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction();
+
+        assert fraction1.toString().equals("2/3");
+        assert fraction2.toString().equals("5/1");
+        assert fraction3.toString().equals("0/1");
+
+        System.out.println(fraction1);
+        System.out.println(fraction2);
+        System.out.println(fraction3);
+
+        Fraction fraction = new Fraction(2, 3);
+
+        assert fraction.getNumerator() == 2;
+        assert fraction.getDenominator() == 3;
+
     }
 }
